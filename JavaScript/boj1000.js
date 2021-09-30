120b0c929c83870fb6be7f44665df0d21f93f90f
seung-u <80995345+cse-mongoose@users.noreply.github.com>
2021-09-30 21:55:00 +0900

    javascript boj

--- JavaScript/boj1000.js
+++ JavaScript/boj1000.js
@@ -2,4 +2,4 @@ let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split(' ');
 
 
-console.log(Number(input[0]) + Number(input[1]))
+console.log(Number(input[0]) + Number(input[1]));
