1cb0102ccd65905e987d238d6677609b8aa20f40
seung-u <80995345+cse-mongoose@users.noreply.github.com>
2021-12-10 00:28:30 +0900

    2021-12-10

---
+++ python/boj11053.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+a = list(map(int, input().split()))
+dp = [1 for i in range(n)]
+
+for i in range(n):
+    for j in range(i):
+        if a[j] < a[i]:
+            dp[i] = max(dp[j] + 1, dp[i])
+
+print(max(dp))
