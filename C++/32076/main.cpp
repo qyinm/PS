3af23c9cd438f10b06d9211faabb1b4ae19ec8bb
seungwoo <mongoose@yu.ac.kr>
2023-01-06 01:10:36 +0900

    second version

--- C++/32076/main.cpp
+++ C++/32076/main.cpp
@@ -8,21 +8,16 @@ int main() {
     cin >> n;
 
     vector<int> data(n);
-    vector<int> cache(n);
+    vector<int> cache(n, 1);
     for (auto &d: data) {
         cin >> d;
     }
 
-    cache[n - 1] = 1;
-    int ans = 1;
-    for (int i = n - 2; i >= 0; i--) {
-        int maxL = 0;
+    int ans = 0;
+    for (int i = n - 1; i >= 0; i--) {
         for (int j = i + 1; j < n; j++) {
-            if (data[i] < data[j]) {
-                maxL = max(cache[j], maxL);
-            }
+            if (data[i] < data[j]) cache[i] = max(cache[j] + 1, cache[i]);
         }
-        cache[i] = maxL + 1;
         ans = max(cache[i], ans);
     }
     cout << ans;
