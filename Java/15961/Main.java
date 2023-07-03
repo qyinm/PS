f3f733516282b052de24372c90e7db01a21cb73b
seungwoo <80995345+iamseungwoo@users.noreply.github.com>
2023-07-03 11:18:08 +0000

    [solve] boj 15961

---
+++ Java/15961/Main.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int variation = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int couponNumber = Integer.parseInt(st.nextToken());
+
+
+        int[] rail = new int[n];
+        for (int i = 0; i < n; i++) {
+            rail[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] check = new int[variation + 1];
+        
+        int distinct = 0;
+        int maxCnt = 0;
+        int flag = 1;
+        int l = 0;
+        int r = 0;
+        int cnt = 0;
+        for(; r < k; r++) {
+            if (check[rail[r%n]]++ == 0)    cnt++;
+        }
+        maxCnt = cnt;
+        if (check[couponNumber] == 0) maxCnt++;
+        
+        
+        while (l < n) {
+            if(--check[rail[l]] == 0)   --cnt;
+            if (check[rail[r%n]]++ == 0)    cnt++;
+            if (check[couponNumber] == 0) {
+                maxCnt = Math.max(maxCnt, cnt + 1);
+            } else {
+                maxCnt = Math.max(maxCnt, cnt);
+            }
+            l++;
+            r++;
+        }
+        System.out.println(maxCnt);
+
+    }
+}
