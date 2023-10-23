7e9a25fe4ce54a5989a31f694aff649bca89c935
seungwoo <80995345+iamseungwoo@users.noreply.github.com>
2023-10-23 19:34:48 +0900

    solve 11722 boj

---
+++ BOJ/11722/Main.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    public void solve() throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        
+        int[] dp = new int[n];
+        int[] data = new int[n];
+        st = new StringTokenizer(br.readLine());
+        
+        for(int i = 0; i < n; i++) {
+            data[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int ans = -1;
+        for (int i = n-1; i >= 0; i--) {
+            int add = 0;
+            for (int j = i+1; j < n; j++) {
+                if (data[i] > data[j]) {
+                    add = Math.max(add, dp[j]);
+                }
+            }
+            dp[i] = add + 1;
+            ans = Math.max(dp[i], ans);
+        }
+        
+        System.out.println(ans);
+    }
+    
+	public static void main(String[] args) throws Exception {
+		new Main().solve();
+	}
+}
