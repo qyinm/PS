67b9a4bc9eb5610ca44df1bd14a033103497cfec
seungwoo <80995345+iamseungwoo@users.noreply.github.com>
2023-11-17 14:45:10 +0900

    solve 2108

---
+++ BOJ/2108/main.py
@@ -0,0 +1,19 @@
+import sys, statistics
+input = sys.stdin.readline
+
+n = int(input())
+data = []
+
+for _ in range(n):
+    temp = int(input())
+    data.append(temp)
+data.sort()
+
+print("%.f"%(round(sum(data) / n)))
+
+print(data[n//2])
+
+result = statistics.multimode(data)
+result = result[1] if len(result) > 1 else result[0]
+print(result)
+print(data[-1] - data[0])
