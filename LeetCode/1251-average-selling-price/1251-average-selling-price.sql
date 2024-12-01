1eaca8c2c5fd973ae0d28df2fda17d2bfd9f61ba
seungwoo <80995345+qyinm@users.noreply.github.com>
2024-12-01 23:19:23 +0900

    Time: 744 ms (80.49%), Space: 0B (100%) - LeetHub

--- LeetCode/1251-average-selling-price/1251-average-selling-price.sql
+++ LeetCode/1251-average-selling-price/1251-average-selling-price.sql
@@ -1,4 +1,4 @@
 select p.product_id, ifnull(round(sum(p.price * u.units) / sum(u.units), 2), 0) as average_price 
 from prices p 
-left join unitssold u on p.product_id = u.product_id and p.start_date <= u.purchase_date and p.end_date >= u.purchase_date
+left join unitssold u on p.product_id = u.product_id and u.purchase_date between p.start_date and p.end_date
 group by p.product_id;
