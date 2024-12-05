40828a336670be197690ab8bae991d01b9da41a0
seungwoo <80995345+qyinm@users.noreply.github.com>
2024-12-05 23:50:20 +0900

    Time: 857 ms (95.95%), Space: 0B (100%) - LeetHub

--- LeetCode/0577-employee-bonus/0577-employee-bonus.sql
+++ LeetCode/0577-employee-bonus/0577-employee-bonus.sql
@@ -1,1 +1,1 @@
-select e.name, b.bonus from employee e left join bonus b on e.empId = b.empId where b.bonus is NULL or b.bonus <= 1000
+select e.name, b.bonus from employee e left join bonus b on e.empId = b.empId where b.bonus is null or b.bonus < 1000
