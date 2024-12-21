b84d01dfb4e828df90f3c64797031d5687a1e056
qyinm <qusseun@gmail.com>
2024-12-21 14:48:28 +0900

    refactor boj 4195

--- BOJ/4195/main.cc
+++ BOJ/4195/main.cc
@@ -4,21 +4,30 @@
 
 using namespace std;
 
-string find(const string s, map<string, string>& m) {
-  if (s == m[s]) return s;
-  return m[s] = find(m[s], m);
+int find(int s, vector<int>& parents) {
+  if (s == parents[s]) return s;
+  return parents[s] = find(parents[s], parents);
 }
 
-void unionRank(string i, string j, map<string, string>& m,
-               map<string, int>& rank) {
+void unionRank(int i, int j, vector<int>& parents, vector<int>& rank) {
+  i = find(i, parents);
+  j = find(j, parents);
+
+  if (i == j) {
+    cout << rank[i] << "\n";
+    return;
+  }
+
   int temp = rank[i] + rank[j];
-  if (rank[i] < rank[j]) {
+  if (rank[i] > rank[j]) {
     rank[i] = temp;
-    m[j] = i;
+    parents[j] = i;
   } else {
     rank[j] = temp;
-    m[i] = j;
+    parents[i] = j;
   }
+
+  cout << temp << "\n";
 }
 
 int main() {
@@ -30,25 +39,25 @@ int main() {
 
   while (tc--) {
     cin >> f;
-    map<string, string> m;
-    map<string, int> rank;
+    map<string, int> m;
+    vector<int> parents(f * 2);
+    vector<int> rank(f * 2, 1);
+    int idx = 0;
     while (f--) {
       string a, b;
       cin >> a >> b;
       if (m.find(a) == m.end()) {
-        m[a] = a;
-        rank[a] = -1;
+        m[a] = idx;
+        parents[idx] = idx;
+        idx += 1;
       }
       if (m.find(b) == m.end()) {
-        m[b] = b;
-        rank[b] = -1;
+        m[b] = idx;
+        parents[idx] = idx;
+        idx += 1;
       }
-      m[a] = find(a, m);
-      m[b] = find(b, m);
-      if (m[a] != m[b]) {
-        unionRank(m[a], m[b], m, rank);
-      }
-      cout << -min(rank[m[a]], rank[m[b]]) << "\n";
+
+      unionRank(m[a], m[b], parents, rank);
     }
   }
 
