aaa60788c638b7e2c3a4cdb4a1383158dc063d88
qyinm <qusseun@gmail.com>
2025-03-20 10:27:34 +0900

    solve boj 30413

---
+++ BOJ/30413/main.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+MOD = 1_000_000_007
+
+def calculate(n, m):
+    if m == 0:
+        return 1
+    if m % 2 == 1:
+        return (n % MOD) * (calculate(n, m - 1) % MOD)
+    ret = calculate(n, m // 2) % MOD
+    return (ret * ret) % MOD
+
+
+def main():
+    A, B = map(int, input().split())
+    if A == 1:
+        print(1)
+        return
+    upside = (calculate(A, B) - 1) % MOD
+    mod_inverse = calculate(A - 1, MOD - 2) % MOD
+    ans = (upside * mod_inverse) % MOD
+    print(ans)
+
+if __name__ == "__main__":
+    main()
