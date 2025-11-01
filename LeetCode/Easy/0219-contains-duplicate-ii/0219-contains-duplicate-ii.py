9d9da77dc00de139fbeceaa399ce0a6816185733
qyinm <80995345+qyinm@users.noreply.github.com>
2025-11-01 19:58:45 +0900

    Time: 41 ms (20.46%), Space: 30.5 MB (96.24%) - LeetHub

--- LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,20 +1,12 @@
-from collections import deque
-
-
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        end = len(nums)
-        windowq = deque(nums[:k+1])
-        count_dict = Counter(windowq)
-        if len(set(windowq)) != len(windowq):
-            return True
-
-        for i in range(k+1, end):
-            byebye = windowq.popleft()
-            count_dict[byebye] -= 1
-            if count_dict[nums[i]] == 1:
+        window = set()
+        
+        for i, num in enumerate(nums):
+            if num in window:
                 return True
-            count_dict[nums[i]] += 1
-            windowq.append(nums[i])
-
+            window.add(num)
+            if len(window) > k:
+                window.remove(nums[i - k])
+        
         return False
