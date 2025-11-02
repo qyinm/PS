a298323282bb490e35d91d4e18a3b12b6df7775e
qyinm <qusseun@gmail.com>
2025-11-02 14:56:40 +0900

    replace location

---
+++ LeetCode/Easy/0202-happy-number/0202-happy-number.kt
@@ -0,0 +1,25 @@
+class Solution {
+    fun isHappy(n: Int): Boolean {
+        val seen = hashSetOf<Int>()
+        var current = n
+        while (current != 1 && current !in seen) {
+            seen.add(current)
+            current = getNextNumber(current)
+        }
+        
+        return current == 1
+    }
+
+    private fun getNextNumber(n: Int): Int {
+        var sum = 0
+        var num = n
+
+        while (num > 0) {
+            val digit = num % 10
+            sum += digit * digit
+            num /= 10
+        }
+
+        return sum
+    }
+}
