b7e5db74f74df01c273b08ab53a40f28eb590739
qyinm <80995345+qyinm@users.noreply.github.com>
2026-02-12 13:22:17 +0900

    Time: 14 ms (80.48%), Space: 19.7 MB (23.45%) - LeetHub

--- LeetCode/Easy/0383-ransom-note/0383-ransom-note.py
+++ LeetCode/Easy/0383-ransom-note/0383-ransom-note.py
@@ -1,22 +1,5 @@
+from collections import Counter
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        compo = {}
-        for note in ransomNote:
-            if note not in compo:
-                compo[note] = 1
-            else:
-                compo[note] += 1
-        
-        compo_mag = {}
-        for mag in magazine:
-            if mag not in compo_mag:
-                compo_mag[mag] = 1
-            else:
-                compo_mag[mag] += 1
-
-        for k, v in compo.items():
-            if k in compo_mag and compo_mag[k] >= v:
-                continue
-            else:
-                return False
-        return True
+        return Counter(ransomNote) - Counter(magazine) == {}
