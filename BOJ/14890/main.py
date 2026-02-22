65a2b629b25c134bd7b60b3f226ef5723c03a27d
qyinm <qusseun@gmail.com>
2026-02-22 22:26:19 +0900

    boj 14890 fix bugs and refactor

--- BOJ/14890/main.py
+++ BOJ/14890/main.py
@@ -2,61 +2,36 @@ import sys
 
 input = sys.stdin.readline
 
+
+def can_pass(line: list[int], l: int) -> bool:
+    n = len(line)
+    j = 0
+    prev_dir = None
+    while j < n:
+        tmp_idx = next((k for k in range(j + 1, n) if line[j] != line[k]), n)
+        if tmp_idx == n:
+            return True
+        next_idx = next((k for k in range(tmp_idx + 1, n) if line[tmp_idx] != line[k]), n)
+        diff = line[tmp_idx] - line[j]
+
+        if diff == 1 and (j + 2 * l if prev_dir == "down" else j + l) <= tmp_idx:
+            prev_dir = "up"
+        elif diff == -1 and tmp_idx + l <= next_idx:
+            prev_dir = "down"
+        else:
+            return False
+        j = tmp_idx
+    return True
+
+
 def main():
     n, l = map(int, input().split())
-    board: list[list[int]] = [list(map(int, input().split())) for _ in range(n)]
+    board = [list(map(int, input().split())) for _ in range(n)]
 
-    ans = 0
+    rows = (board[i] for i in range(n))
+    cols = ([board[j][i] for j in range(n)] for i in range(n))
+    print(sum(can_pass(line, l) for line in (*rows, *cols)))
 
-    for i in range(n):
-        flag = True
-        j = 0
-        prev_dir = None
-        while j < n:
-            tmp_idx = next((k for k in range(j + 1, n) if board[i][j] != board[i][k]), n)
-            next_idx = next((k for k in range(tmp_idx + 1, n) if board[i][tmp_idx] != board[i][k]), n)
-            if tmp_idx == n:
-                break
-            if abs(board[i][j] - board[i][tmp_idx]) > 1:
-                flag = False
-                break
-            if board[i][j] < board[i][tmp_idx] and (j + l + l if prev_dir == "down" else j + l) <= tmp_idx:
-                j = tmp_idx
-                prev_dir = "up"
-                continue
-            if board[i][j] > board[i][tmp_idx] and tmp_idx + l <= next_idx:
-                j = tmp_idx
-                prev_dir = "down"
-                continue
-            flag = False
-            break
-        if flag:
-            ans += 1
-    for i in range(n):
-        flag = True
-        j = 0
-        prev_dir = None
-        while j < n:
-            tmp_idx = next((k for k in range(j + 1, n) if board[j][i] != board[k][i]), n)
-            next_idx = next((k for k in range(tmp_idx + 1, n) if board[tmp_idx][i] != board[k][i]), n)
-            if tmp_idx == n:
-                break
-            if abs(board[j][i] - board[tmp_idx][i]) > 1:
-                flag = False
-                break
-            if board[j][i] < board[tmp_idx][i] and (j + l + l if prev_dir == "down" else j + l) <= tmp_idx:
-                j = tmp_idx
-                prev_dir = "up"
-                continue
-            if board[j][i] > board[tmp_idx][i] and tmp_idx + l <= next_idx:
-                j = tmp_idx
-                prev_dir = "down"
-                continue
-            flag = False
-            break
-        if flag:
-            ans += 1
-    print(ans)
 
 if __name__ == "__main__":
     main()
